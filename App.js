323ad23aff1c70f523030e3ab25700db9cac7021
Adu Danso <adanso030@st.ug.edu.gh>
2024-07-05 21:11:24 +0000

    made my name bold in App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Adu Danso</Text>
+      <Text style={styles.text}>My name is  <Text style={styles.boldText}>Adu Danso</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +20,7 @@ const styles = StyleSheet.create({
   text:{
     fontSize: 24,
   },
-
+  boldText:{
+    fontWeight: 'bold',
+  },
 });
